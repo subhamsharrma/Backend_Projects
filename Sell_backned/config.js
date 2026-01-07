6ad19cf976b426cba4c2f02bf03e1b3258f27e8d
Subham_Sharrma <subhamsharrma@gmail.com>
2026-01-07 18:46:23 +0530

    Added

--- Sell_backned/config.js
+++ Sell_backned/config.js
@@ -1,5 +1,5 @@
 const JWT_USER_PASSWORD = process.env.JWT_USER_PASSWORD;
-const JWT_ADMIN_PASSWORD = process.env.JWT_ADMIN_PASSWORDssysytsy;
+const JWT_ADMIN_PASSWORD = process.env.JWT_ADMIN_PASSWORD;
 
 module.exports = {
     JWT_ADMIN_PASSWORD,
